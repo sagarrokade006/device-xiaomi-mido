781970faf48f935c081ce1f76726dcd5f09202b5
LuK1337 <priv.luk@gmail.com>
2020-01-14 19:02:40 +0100

    mido: Enable ro.telephony.use_old_mnc_mcc_format
    Change-Id: I46e510d35ad0cf2befebbf3e1530e30a2f2e4f71
    Signed-off-by: smokey18 <kmboplays@gmail.com>

--- prop.mk
+++ prop.mk
@@ -165,6 +165,7 @@ rild.libargs=-d/dev/smd0 \
 rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
 ro.telephony.call_ring.multiple=false \
 ro.telephony.default_network=22,20 \
+ro.telephony.use_old_mnc_mcc_format=true \
 service.qti.ims.enabled=1 \
 telephony.lteOnCdmaDevice=1 \
 persist.sys.fflag.override.settings_network_and_internet_v2=true
